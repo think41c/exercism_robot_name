830a047ee8b838bc9aaf5cc93b85ff07481b8cb0
Tony DiNitto <think41c@gmail.com>
2015-03-26 20:32:51 -0600

    Random name generated.

--- robot.rb
+++ robot.rb
@@ -1,8 +1,14 @@
 class Robot
 
   def initialize 
+    first_letter  = (65 + rand(26)).chr
+    second_letter = (65 + rand(26)).chr
+    first_num     = rand(10)
+    second_num    = rand(10)
+    third_num     = rand(10)
+    puts "#{first_letter}#{second_letter}#{first_num}#{second_num}#{third_num}"
   end
-  
+
   def name 
     "RX123"
   end
