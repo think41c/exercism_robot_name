e700c992225537ec71631ac843b82fb904ff7a3b
Tony DiNitto <think41c@gmail.com>
2015-03-31 22:45:18 -0600

    Refactored - extracted methods. Created more random robot namnes.

--- robot.rb
+++ robot.rb
@@ -3,20 +3,21 @@ class Robot
   attr_reader :name
 
   def initialize 
-    robot_name_numb = rand(10)
-    rand_char = ('A'..'Z').to_a.sample
-
-    first_letter  = rand_char
-    second_letter = rand_char
-    first_num     = robot_name_numb
-    second_num    = robot_name_numb
-    third_num     = robot_name_numb
-    
-    @name = "#{first_letter}#{second_letter}#{first_num}#{second_num}#{third_num}"
+    create_name
   end
 
   def reset
-    @name = Robot.new.name
+    create_name
+  end
+
+  def create_name
+    first_letter  = ('A'..'Z').to_a.sample
+    second_letter = ('A'..'Z').to_a.sample
+    first_num     = rand(10)
+    second_num    = rand(10)
+    third_num     = rand(10)
+    
+    @name = "#{first_letter}#{second_letter}#{first_num}#{second_num}#{third_num}"
   end
 
 end
